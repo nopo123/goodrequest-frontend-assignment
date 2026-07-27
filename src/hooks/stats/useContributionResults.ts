5fd8a37099fa29132a46ab6c2fdc7bd48f2b9739
Patrik Karas <patrik.karas@beka-software.at>
2026-07-27 14:48:00 +0200

    style: standardize quotation marks and formatting in useContributionResults hook

--- src/hooks/stats/useContributionResults.ts
+++ src/hooks/stats/useContributionResults.ts
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 
-import { sheltersApi } from '@/api/shelters';
-import { queryKeys } from '@/lib/query/query-keys';
+import { sheltersApi } from "@/api/shelters";
+import { queryKeys } from "@/lib/query/query-keys";
 
 export const useContributionResults = (search?: string) =>
   useQuery({
     queryKey: queryKeys.sheltersResults(search),
     queryFn: ({ signal }) => sheltersApi.getResults(search, signal),
+    refetchOnWindowFocus: true,
   });
